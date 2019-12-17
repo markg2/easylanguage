fd5bbff07f3e28c87c99cc4701c71465663d0c2c
unknown <markg2@att.net>
2019-12-17 07:46:09 -0800

    IND: imbalance logic added; poc index persisted but not used

--- mg-volannot.el
+++ mg-volannot.el
@@ -24,9 +24,13 @@ var:
 	Dictionary dictBid(null),
 	Dictionary dictAsk(null),
 	IntrabarPersist int bidCumulative(0),
-	IntrabarPersist int askCumulative(0);
+	IntrabarPersist int askCumulative(0),
+	IntrabarPersist int pocIndex(1),
+	IntrabarPersist int bidImbalanceCount(0),
+	IntrabarPersist int askImbalanceCount(0)
+	; 
 
-Method Dictionary process(double argsPrice, int argsSize, Dictionary tsDict, TimeAndSalesItemTickType tickType)
+Method Dictionary persistBidAsk(double argsPrice, int argsSize, Dictionary tsDict, TimeAndSalesItemTickType tickType)
 var: int tradeVol, String keyPrice, int x, string calcValue;
 Begin
 	tradeVol = argsSize;
@@ -61,14 +65,15 @@ begin
 			Begin
 				If price = lastBid Then begin
 					bidCumulative = bidCumulative + args.Data.Size;
-					process(args.Data.Price, args.Data.Size, dictBid, TimeAndSalesItemTickType.Bid);
-			 		process(args.Data.Price, 0, dictAsk, TimeAndSalesItemTickType.Bid);
+					persistBidAsk(args.Data.Price, args.Data.Size, dictBid, TimeAndSalesItemTickType.Bid);
+			 		persistBidAsk(args.Data.Price, 0, dictAsk, TimeAndSalesItemTickType.Bid);
 				end;
 				If price = lastAsk Then begin
 					askCumulative = askCumulative + args.Data.Size;
-					process(args.Data.Price, args.Data.Size, dictAsk,  TimeAndSalesItemTickType.Ask);
-					process(args.Data.Price, 0, dictBid,  TimeAndSalesItemTickType.Ask);
+					persistBidAsk(args.Data.Price, args.Data.Size, dictAsk,  TimeAndSalesItemTickType.Ask);
+					persistBidAsk(args.Data.Price, 0, dictBid,  TimeAndSalesItemTickType.Ask);
 				end;
+				calcHighsAndPoc(dictBid, dictAsk);
 			end;
 		end
 		else if type = TimeAndSalesItemTickType.Ask Then begin
@@ -82,15 +87,108 @@ begin
 	end ;
 end;
 
-Method void plotVolume(int bidC, int askC, double hStrike, double lStrike)
+Method void calcHighsAndPoc(Dictionary dictBid, Dictionary dictAsk)
+var: 	string keyPrice, string calcValue,
+		string calcValue2,
+		int bidCount, int askCount, int highestCount,
+		int x, int bidIndex, int askIndex,
+		int pocVolHigh,
+		int pocVolTemp,
+		int counterAscending;
+
 Begin
+	bidCount = dictBid.Count;
+	askCount = dictAsk.Count;
+	highestCount = askCount;
+	pocVolHigh = 0;
+	pocVolTemp = 0;
+	counterAscending = 1;
+	bidImbalanceCount = 0;
+	askImbalanceCount = 0;
 	
+	If bidCount > askCount Then highestCount = bidCount;
+	x = highestCount;
+	
+	while x >= 0 Begin
+		calcValue = "0";
+		calcValue2 = "0";
+		bidIndex = x + 1;
+		askIndex = x;
+		
+		If bidIndex <= dictBid.Count And bidIndex > 0 Then Begin
+			keyPrice = dictBid.Keys[bidIndex - 1].ToString();
+			calcValue = dictBid.Items[keyPrice].ToString();
+			bidImbalanceCount = bidImbalanceCount +
+				imbalanceCount(dictBid.Items[keyPrice].ToString(), dictAsk.Items[keyPrice].ToString(), 1);
+		end;
+
+		If askIndex <= dictAsk.Count And askIndex > 0 Then Begin
+			keyPrice = dictAsk.Keys[askIndex - 1].ToString();
+			calcValue2 = dictAsk.Items[keyPrice].ToString();
+			askImbalanceCount = askImbalanceCount +
+				imbalanceCount(dictBid.Items[keyPrice].ToString(), dictAsk.Items[keyPrice].ToString(), 0);
+		end;
+
+		// point of control
+		pocVolTemp = Strtonum(calcValue) + Strtonum(calcValue2);
+		if pocVolTemp > pocVolHigh then Begin
+		 	pocIndex = counterAscending;
+	 		pocVolHigh = pocVolTemp;
+	 		pocVolTemp = 0;
+		end;
+
+		x = x - 1;
+		counterAscending = counterAscending + 1;
+	end;
+	
+	// display point of control
+	{
+	if pocIndex < DataGridView1.Rows.CountTotal and pocIndex > 0 then begin
+		row = DataGridView1.Rows.at(pocIndex);
+		row.Cells[4].Value = "#";
+	end;
+	}	
+
+end;
+
+Method int imbalanceCount(String strBid, String strAsk, int isBid)
+var: 	double quotient, int bid, int ask, int retval;
+Begin
+	retval = 0;
+	bid = Strtonum(strBid);
+	ask = Strtonum(strAsk);
+
+	If isBid >= 1 And ask > 0 Then Begin
+		quotient = bid/ask;
+		If quotient > 4 Then retval = 1;
+	end 
+	else if isBid >= 1 and  bid >=400 then begin
+		retval = 1;
+	end;
+
+	If isBid < 1 And bid > 0 Then Begin
+		quotient = ask/bid;
+		If quotient > 4 Then retval = 1;
+	end 
+	else if isBid < 1 and ask >=400 then begin
+		retval = 1;
+	end;
+	
+	return retval;
+end;
+
+
+Method void plotVolume(int bidC, int askC, double hStrike, double lStrike)
+var: string imbalances;
+Begin
+	imbalances = numtostr(bidImbalanceCount, 0) + "x" + numtostr(askImbalanceCount, 0);
+
 	if bidC > askC then begin
 		tBid = TextLabel.Create(BNPoint.Create(BarNumber, lStrike), "-" + numtostr(bidC - askC, 0));
 		tBid.Color = Color.Red;
 		tBid.Persist = true;		// persist keeps the text label on the chart between tick updates
 		DrawingObjects.Add(tBid);	// draws the text on the chart
-		tAsk =  TextLabel.Create(BNPoint.Create(BarNumber, hStrike), "0x0");
+		tAsk =  TextLabel.Create(BNPoint.Create(BarNumber, hStrike), imbalances);
 		tAsk.Color = Color.Red;
 		tAsk.Persist = true;
 		DrawingObjects.Add(tAsk);
@@ -101,7 +199,7 @@ Begin
 		tAsk.Color = Color.LightBlue;
 		tAsk.Persist = true;		// persist keeps the text label on the chart between tick updates
 		DrawingObjects.Add(tAsk);	// draws the text on the chart
-		tBid =  TextLabel.Create(BNPoint.Create(BarNumber, lStrike), "0x0");
+		tBid =  TextLabel.Create(BNPoint.Create(BarNumber, lStrike), imbalances);
 		tBid.Color = Color.LightBlue;
 		tBid.Persist = true;
 		DrawingObjects.Add(tBid);
